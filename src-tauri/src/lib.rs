55f369a5fd5d84d7d9492006b9d345590bc86ff5
Archinme <brokenmopflag@gmail.com>
2025-10-01 19:22:50 +0100

    Added incomplete rust rusqlite to backend

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,12 +1,21 @@
+use rusqlite::Connections;
+
 #[tauri::command]
 fn hello() -> String {
     "Hello from Rust!".to_string()
 }
 
+//Get everything from tasks
+#[tauri::command]
+fn getFromTasks(){
+    let conn = Connections::open("../database/database.sql");
+}
+
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![hello])
+        .invoke_handler(tauri::generate_handler![hello, getFromTasks])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
